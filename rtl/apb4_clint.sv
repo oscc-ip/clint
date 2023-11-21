fac39dd451bd866af4b32eacf069b34c69fc58af
maksyuki <maksyuki@126.com>
2023-11-21 15:50:58 +0800

    feat: add comp script and modify rtl modules

--- rtl/apb4_clint.sv
+++ rtl/apb4_clint.sv
@@ -8,92 +8,83 @@
 // MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
-// verilog_format: off
-`define CLINT_MSIP      4'b0000 //BASEADDR+0x00
-`define CLINT_MTIMEL    4'b0001 //BASEADDR+0x04
-`define CLINT_MTIMEH    4'b0010 //BASEADDR+0x08
-`define CLINT_MTIMECMPL 4'b0011 //BASEADDR+0x0C
-`define CLINT_MTIMECMPH 4'b0100 //BASEADDR+0x10
-// verilog_format: on
+`include "register.sv"
+`include "edge_det.sv"
+`include "clint_define.sv"
 
 module apb4_clint (
-    // verilog_format: off
     apb4_if.slave apb4,
-    // verilog_format: on
-    input  logic  rtc_clk_i,
-    output logic  tmr_irq_o,
-    output logic  sfr_irq_o
+    clint_if.dut  clint
 );
 
-  logic [3:0] s_apb_addr;
-  logic [31:0] s_msip_d, s_msip_q;
-  logic [63:0] s_mtime_d, s_mtime_q, s_mtimecmp_d, s_mtimecmp_q;
-  logic s_rtc_rise_edge;
-  logic s_apb4_wr_valid, s_msip_wr_valid;
-  logic s_mtimecmpl_wr_valid, s_mtimecmph_wr_valid, s_mtimecmp_wr_valid;
+  logic [3:0] s_apb4_addr;
+  logic [`CLINT_MSIP_WIDTH-1:0] s_msip_d, s_msip_q;
+  logic [`CLINT_MTIME_WIDTH-1:0] s_mtime_d, s_mtime_q;
+  logic [`CLINT_MTIMECMP_WIDTH-1:0] s_mtimecmp_d, s_mtimecmp_q;
+  logic s_rtc_rise_edge, s_apb4_wr_hdshk, s_apb4_rd_hdshk;
 
-  assign s_apb4_wr_valid = (apb4.psel && apb4.penable) && apb4.pwrite;
-  assign s_apb4_rd_valid = (apb4.psel && apb4.penable) && (~apb4.pwrite);
-  assign s_msip_wr_valid = s_apb4_wr_valid && (s_apb_addr == `CLINT_MSIP);
+  assign s_apb4_addr     = apb4.paddr[5:2];
+  assign s_apb4_wr_hdshk = (apb4.psel && apb4.penable) && apb4.pwrite;
+  assign s_apb4_rd_hdshk = (apb4.psel && apb4.penable) && (~apb4.pwrite);
+  assign apb4.pready     = 1'b1;
+  assign apb4.pslverr    = 1'b0;
 
-  edge_det u_edge_det (
-      .clk_i  (apb4.hclk),
-      .rst_n_i(apb4.hresetn),
-      .dat_i  (rtc_clk_i),
+  edge_det #(2, 1) u_edge_det (
+      .clk_i  (apb4.pclk),
+      .rst_n_i(apb4.presetn),
+      .dat_i  (clint.rtc_clk_i),
       .re_o   (s_rtc_rise_edge)
   );
 
-  assign s_apb_addr = apb4.paddr[5:2];
-  always_comb begin
-    apb4.prdata = '0;
-    if (s_apb4_rd_valid) begin
-      unique case (s_apb_addr)
-        `CLINT_MSIP:      apb4.prdata = s_msip_q;
-        `CLINT_MTIMEL:    apb4.prdata = s_mtime_q[31:0];
-        `CLINT_MTIMEH:    apb4.prdata = s_mtime_q[63:32];
-        `CLINT_MTIMECMPL: apb4.prdata = s_mtimecmp_q[31:0];
-        `CLINT_MTIMECMPH: apb4.prdata = s_mtimecmp_q[63:32];
-      endcase
-    end
-  end
-
-  assign s_msip_d = s_msip_wr_valid ? apb4.pwdata : s_msip_q;
-  dffr #(32) u_msip_dffr (
-      apb4.hclk,
-      apb4.hresetn,
+  assign s_msip_d = s_apb4_wr_hdshk && (s_apb4_addr == `CLINT_MSIP) ? apb4.pwdata[`CLINT_MSIP_WIDTH-1:0] : s_msip_q;
+  dffr #(`CLINT_MSIP_WIDTH) u_msip_dffr (
+      apb4.pclk,
+      apb4.presetn,
       s_msip_d,
       s_msip_q
   );
 
   assign s_mtime_d = s_rtc_rise_edge ? s_mtime_q + 1'b1 : s_mtime_q;
-  dffr #(64) u_mtime_dffr (
-      apb4.hclk,
-      apb4.hresetn,
+  dffr #(`CLINT_MTIME_WIDTH) u_mtime_dffr (
+      apb4.pclk,
+      apb4.presetn,
       s_mtime_d,
       s_mtime_q
   );
 
   always_comb begin
     s_mtimecmp_d = s_mtimecmp_q;
-    if (s_apb4_wr_valid) begin
-      unique case (s_apb_addr)
+    if (s_apb4_wr_hdshk) begin
+      unique case (s_apb4_addr)
         `CLINT_MTIMECMPL: s_mtimecmp_d = {32'b0, apb4.pwdata};
         `CLINT_MTIMECMPH: s_mtimecmp_d = {apb4.pwdata, 32'b0};
+        default:          s_mtimecmp_d = s_mtimecmp_q;
       endcase
     end
   end
 
-  dffrh #(64) u_mtimecmp_dffrh (
-      apb4.hclk,
-      apb4.hresetn,
+  dffrh #(`CLINT_MTIMECMP_WIDTH) u_mtimecmp_dffrh (
+      apb4.pclk,
+      apb4.presetn,
       s_mtimecmp_d,
       s_mtimecmp_q
   );
 
-  assign tmr_irq_o   = s_mtime_q >= s_mtimecmp_q;
-  assign sfr_irq_o   = s_msip_q[0] == 1'b1;
+  assign clint.tmr_irq_o = s_mtime_q >= s_mtimecmp_q;
+  assign clint.sfr_irq_o = s_msip_q[0] == 1'b1;
 
-  assign apb4.pready = 1'b1;
-  assign apb4.pslerr = 1'b0;
+  always_comb begin
+    apb4.prdata = '0;
+    if (s_apb4_rd_hdshk) begin
+      unique case (s_apb4_addr)
+        `CLINT_MSIP:      apb4.prdata[`CLINT_MSIP_WIDTH-1:0] = s_msip_q;
+        `CLINT_MTIMEL:    apb4.prdata = s_mtime_q[31:0];
+        `CLINT_MTIMEH:    apb4.prdata = s_mtime_q[63:32];
+        `CLINT_MTIMECMPL: apb4.prdata = s_mtimecmp_q[31:0];
+        `CLINT_MTIMECMPH: apb4.prdata = s_mtimecmp_q[63:32];
+        default:          apb4.prdata = '0;
+      endcase
+    end
+  end
 
 endmodule
---
+++ rtl/clint_define.sv
@@ -0,0 +1,68 @@
+// Copyright (c) 2023 Beijing Institute of Open Source Chip
+// clint is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+`ifndef INC_CLINT_DEF_SV
+`define INC_CLINT_DEF_SV
+
+/* register mapping
+ * CLINT_MSIP:
+ * BITS:   | 31:1  | 0    |
+ * FIELDS: | RES   | msip |
+ * PERMS:  | NONE  | RW   |
+ * ------------------------
+ * CLINT_MTMEL:
+ * BITS:   | 31:0   |
+ * FIELDS: | MTIMEL |
+ * PERMS:  | RW     |
+ * ------------------------
+ * CLINT_MTIMEH:
+ * BITS:   | 31:0   |
+ * FIELDS: | MTIMEH |
+ * PERMS:  | RW     |
+ * ------------------------
+ * CLINT_MTIMECMPL:
+ * BITS:   | 31:0      |
+ * FIELDS: | MTIMECMPL |
+ * PERMS:  | RW        |
+ * ------------------------
+ * CLINT_MTIMECMPH:
+ * BITS:   | 31:0      |
+ * FIELDS: | MTIMECMPH |
+ * PERMS:  | RW        |
+ * ------------------------
+*/
+
+// verilog_format: off
+`define CLINT_MSIP      4'b0000 // BASEADDR + 0x00
+`define CLINT_MTIMEL    4'b0001 // BASEADDR + 0x04
+`define CLINT_MTIMEH    4'b0010 // BASEADDR + 0x08
+`define CLINT_MTIMECMPL 4'b0011 // BASEADDR + 0x0C
+`define CLINT_MTIMECMPH 4'b0100 // BASEADDR + 0x10
+
+
+`define CLINT_MSIP_ADDR      {26'b0, `CLINT_MSIP     , 2'b00}
+`define CLINT_MTIMEL_ADDR    {26'b0, `CLINT_MTIMEL   , 2'b00}
+`define CLINT_MTIMEH_ADDR    {26'b0, `CLINT_MTIMEH   , 2'b00}
+`define CLINT_MTIMECMPL_ADDR {26'b0, `CLINT_MTIMECMPL, 2'b00}
+`define CLINT_MTIMECMPH_ADDR {26'b0, `CLINT_MTIMECMPH, 2'b00}
+
+`define CLINT_MSIP_WIDTH     1
+`define CLINT_MTIME_WIDTH    64
+`define CLINT_MTIMECMP_WIDTH 64
+
+interface clint_if(input logic rtc_clk_i);
+    logic  tmr_irq_o;
+    logic  sfr_irq_o;
+    
+    modport dut(input rtc_clk_i, output tmr_irq_o, output sfr_irq_o);
+    modport tb(input rtc_clk_i, input tmr_irq_o, input sfr_irq_o);
+endinterface
+// verilog_format: on
+`endif
