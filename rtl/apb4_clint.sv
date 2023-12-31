47d0944512404af776e74cbb5dc1cb7635602204
maksyuki <maksyuki@126.com>
2023-12-31 16:23:22 +0800

    fix: fix a error mtimecmp reg write

--- rtl/apb4_clint.sv
+++ rtl/apb4_clint.sv
@@ -18,10 +18,14 @@ module apb4_clint (
 );
 
   logic [3:0] s_apb4_addr;
+  logic s_apb4_wr_hdshk, s_apb4_rd_hdshk;
   logic [`CLINT_MSIP_WIDTH-1:0] s_msip_d, s_msip_q;
+  logic s_msip_en;
   logic [`CLINT_MTIME_WIDTH-1:0] s_mtime_d, s_mtime_q;
+  logic s_mtime_en;
   logic [`CLINT_MTIMECMP_WIDTH-1:0] s_mtimecmp_d, s_mtimecmp_q;
-  logic s_rtc_rise_edge, s_apb4_wr_hdshk, s_apb4_rd_hdshk;
+  logic s_mtimecmp_en;
+  logic s_rtc_rise_edge;
 
   assign s_apb4_addr     = apb4.paddr[5:2];
   assign s_apb4_wr_hdshk = (apb4.psel && apb4.penable) && apb4.pwrite;
@@ -29,49 +33,55 @@ module apb4_clint (
   assign apb4.pready     = 1'b1;
   assign apb4.pslverr    = 1'b0;
 
-  edge_det #(2, 1) u_edge_det (
+  edge_det_re #(2, 1) u_edge_det_re (
       .clk_i  (apb4.pclk),
       .rst_n_i(apb4.presetn),
       .dat_i  (clint.rtc_clk_i),
       .re_o   (s_rtc_rise_edge)
   );
 
-  assign s_msip_d = s_apb4_wr_hdshk && (s_apb4_addr == `CLINT_MSIP) ? apb4.pwdata[`CLINT_MSIP_WIDTH-1:0] : s_msip_q;
-  dffr #(`CLINT_MSIP_WIDTH) u_msip_dffr (
+  assign s_msip_en = s_apb4_wr_hdshk && s_apb4_addr == `CLINT_MSIP;
+  assign s_msip_d  = s_msip_en ? apb4.pwdata[`CLINT_MSIP_WIDTH-1:0] : s_msip_q;
+  dffer #(`CLINT_MSIP_WIDTH) u_msip_dffer (
       apb4.pclk,
       apb4.presetn,
+      s_msip_en,
       s_msip_d,
       s_msip_q
   );
 
-  assign s_mtime_d = s_rtc_rise_edge ? s_mtime_q + 1'b1 : s_mtime_q;
-  dffr #(`CLINT_MTIME_WIDTH) u_mtime_dffr (
+  assign s_mtime_en = s_rtc_rise_edge;
+  assign s_mtime_d  = s_mtime_en ? s_mtime_q + 1'b1 : s_mtime_q;
+  dffer #(`CLINT_MTIME_WIDTH) u_mtime_dffer (
       apb4.pclk,
       apb4.presetn,
+      s_mtime_en,
       s_mtime_d,
       s_mtime_q
   );
 
+  assign s_mtimecmp_en = s_apb4_wr_hdshk && (s_apb4_addr == `CLINT_MTIMECMPL || s_apb4_addr == `CLINT_MTIMECMPH);
   always_comb begin
     s_mtimecmp_d = s_mtimecmp_q;
     if (s_apb4_wr_hdshk) begin
       unique case (s_apb4_addr)
-        `CLINT_MTIMECMPL: s_mtimecmp_d = {32'b0, apb4.pwdata};
-        `CLINT_MTIMECMPH: s_mtimecmp_d = {apb4.pwdata, 32'b0};
+        `CLINT_MTIMECMPL: s_mtimecmp_d = {s_mtimecmp_q[63:32], apb4.pwdata};
+        `CLINT_MTIMECMPH: s_mtimecmp_d = {apb4.pwdata, s_mtimecmp_q[31:0]};
         default:          s_mtimecmp_d = s_mtimecmp_q;
       endcase
     end
   end
 
-  dffrh #(`CLINT_MTIMECMP_WIDTH) u_mtimecmp_dffrh (
+  dfferh #(`CLINT_MTIMECMP_WIDTH) u_mtimecmp_dfferh (
       apb4.pclk,
       apb4.presetn,
+      s_mtimecmp_en,
       s_mtimecmp_d,
       s_mtimecmp_q
   );
 
   assign clint.tmr_irq_o = s_mtime_q >= s_mtimecmp_q;
-  assign clint.sfr_irq_o = s_msip_q[0] == 1'b1;
+  assign clint.sfr_irq_o = s_msip_q[0];
 
   always_comb begin
     apb4.prdata = '0;
